81fee13063a4a2e695c444ca4f9f90690d9526fd
ulldecorb <ausias.bertran@gmail.com>
2021-11-23 19:22:01 +0100

    Create todoadd component

---
+++ todos/src/components/TodoAdd.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="add-container">
+        <form @submit="addTodo">
+            <input type="text" v-model="title">
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TodoAdd',
+    data(){
+        return {
+            title: ''
+        }
+    },
+    methods: {
+        addTodo(e){
+            e.preventDefault();
+            let idCounter = 3;
+            const newTodo = {
+                id: idCounter,
+                title: this.title,
+                copleted: false,
+            }
+            idCounter ++;
+            this.title='';
+            this.$emit('add-todo', newTodo);
+        }
+    }
+}
+</script>
+
+<style scoped>
+#add-container {
+    padding: 10px;
+}
+
+input {
+    padding: 10px;
+    outline: none;
+    border: solid 1px #ccc;
+    width: 100%;
+}
+</style>
